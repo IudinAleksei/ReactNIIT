efd6df7e43663c593b2e0ab455eb6dbf22b7d263
Iudin_Aleksei <aviordream@gmail.com>
2020-12-19 15:42:36 +0300

    feat: add to input to component

---
+++ calculator/src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Calculator.module.css'
+
+const Calculator = props => (
+  <div className={classes.Calculator}>
+    <input type="number"></input>
+    
+    <input type="number"></input>       
+  </div>
+)
+
+export default Calculator
